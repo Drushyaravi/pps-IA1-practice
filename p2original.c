9e980b4c89a137098fc97f41de185d0b4f53fc26
Drushya M R <95674916+Drushyaravi@users.noreply.github.com>
2022-02-04 04:47:53 +0000

    p2originaldone

---
+++ p2original.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+int input()
+{
+  int a, b, c;
+  printf("Enter a number\n");
+  scanf("%d" , &a);
+  return a;
+}
+int cmp (int a, int b, int c)
+{
+  int result;
+  if (a>b && a>c)
+
+  result=1;
+
+  else if(b>a && b>c)
+
+  result=2;
+  
+  else if(c>a && c>b)
+
+  result=3;
+
+  else if(a==b && a==c)
+
+  result=4;
+
+  else
+
+  result=0;
+
+  return result;
+}
+void output(int result)
+{
+  if (result==1)
+
+  printf("a is greater\n");
+
+  else if(result==2)
+
+  printf("b is greater\n");
+
+  else if(result==3)
+
+  printf("c is greater\n");
+
+  else if(result==4)
+
+  printf("All are Equal\n");
+
+  else
+
+  printf("invalid");
+
+}
+int main()
+{
+  int a, b, c, result;
+  a=input();
+  b=input();
+  c=input();
+  result=cmp(a, b, c);
+  output(result);
+  return 0;
+}
